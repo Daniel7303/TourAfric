f91b21f6901de34ae087fe8ace4e79a843e92871
Daniel7303 <danieleistien@gmail.com>
2023-12-09 21:54:23 +0100

    added a Hanburger icon

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function toggleNav() {
-    var nav = document.querySelector('ul');
-    nav.style.display = (nav.style.display === 'none' || nav.style.display === '') ? 'flex' : 'none';
-  }
-  
+  var nav = document.querySelector('ul');
+  nav.style.display = (nav.style.display === 'none' || nav.style.display === '') ? 'flex' : 'none';
+}
