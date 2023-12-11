5139461910a4e854f47376c02b8d40c4f3405873
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-12-11 11:51:11 +0100

    I modified the contact section and some minor edits

--- Javascript/form.js
+++ Javascript/form.js
@@ -16,3 +16,19 @@ function simpleToast() {
   }, 3000);
 }
 
+
+function togglePasswordVisibility() {
+  const passwordInput = document.getElementById('password');
+  const icon = document.querySelector('.toggle-password');
+
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    icon.classList.remove('fa-eye');
+    icon.classList.add('fa-eye-slash');
+  } else {
+    passwordInput.type = 'password';
+    icon.classList.remove('fa-eye-slash');
+    icon.classList.add('fa-eye');
+  }
+}
+
